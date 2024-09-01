73b6eef3ed5e853719ea91ba2d257434cef245ad
Aryan Jhaveri <96702278+Aryan-Jhaveri@users.noreply.github.com>
2024-09-01 17:28:56 -0400

    Create mashupUtils.ts

---
+++ mashupUtils.ts
@@ -0,0 +1,6 @@
+import metadata from './metadata.json';
+
+export const generateMashup = (emoji1: string, emoji2: string): string => {
+  // Logic to look up the mashup in the metadata
+  // Return the URL or identifier for the mashup image
+};
