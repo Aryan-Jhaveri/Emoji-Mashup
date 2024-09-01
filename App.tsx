cda902797344fa5758acdd376c6145dcdb69471c
Aryan Jhaveri <96702278+Aryan-Jhaveri@users.noreply.github.com>
2024-09-01 17:29:13 -0400

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MashupGenerator from './components/MashupGenerator';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Emoji Mashup Generator</h1>
+      <MashupGenerator />
+    </div>
+  );
+};
+
+export default App;
